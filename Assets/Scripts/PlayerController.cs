9533ed9d7ea2bbd53aa3d60dba2035a333745efb
sofaprusik <sofiia.prusik@oa.edu.ua>
2020-10-27 10:26:24 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
---
+++ Assets/Scripts/TerrainLoop.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TerrainLoop : MonoBehaviour
+{
+    public GameObject spriteShapeGameObject;
+
+    private SpriteShapeController spriteShapeController;
+
+    public float offsetRight = 0;
+
+    private float x1;
+    private float x2;
+
+    public float width;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteShapeController = spriteShapeGameObject.GetComponent<SpriteShapeController>();
+
+        updateEdgesCoordinates();
+
+        height = Camera.main.orthographicSize * 2;
+        width = Camera.main.aspect * height;
+
+        //spriteShapeController.spline.SetPosition(0, new Vector3(-100, 0));
+
+        //spriteShapeController.spline.SetRightTangent(0, new Vector3(1, 3));
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.position.x);
+
+        if (transform.position.x + (width / 2) > (x2 + offsetRight ))
+        {
+            generateNextNodes();
+            updateEdgesCoordinates();
+        }
+
+        if(transform.position.x > (x1 + 100))
+        {
+            removeLeftNodes();
+            updateEdgesCoordinates();
+        }
+    }
+
+    void generateNextNodes()
+    {
+        int length = spriteShapeController.spline.GetPointCount();
+
+        float newY = Random.RandomRange(1.1f, 2.0f);
+
+        float newX = x2 + 10;
+
+        spriteShapeController.spline.InsertPointAt(length, new Vector3(newX, newY, 0));
+
+        //spriteShapeController.spline.SetTangentMode(length, ShapeTangentMode.Continuous);
+
+        //spriteShapeController.spline.SetLeftTangent(length, new Vector3(newX - 1, newY, 0));
+
+        //spriteShapeController.spline.SetRightTangent(length, new Vector3(newX + 1, newY, 0));
+
+    }
+
+    void removeLeftNodes()
+    {
+        //
+    }
+
+    void updateEdgesCoordinates()
+    {
+        Vector3 leftPosition = spriteShapeController.spline.GetPosition(0);
+        x1 = leftPosition.x;
+
+        int length = spriteShapeController.spline.GetPointCount();
+        Vector3 rightPosition = spriteShapeController.spline.GetPosition(length - 1);
+        x2 = rightPosition.x;
+
+    }
+}
